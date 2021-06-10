c91fc140f0ebcc919a886c2e596047f6970b1975
Martial Wehrling <d.wehrling@gmail.com>
2021-06-10 16:17:11 +0200

    modification HTLM and CSS. Start script.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// choice table multiply
+const tableMultiply = document.querySelectorAll('.btn-number');
+
+// choice table multiply random
+const buttonTableRandom = document.querySelector('.table-number-random');
+const tableMultiplyRandom = document.getElementById('multiply-random');
+
+//choice number operation
+const choiceOperation = document.querySelector(
+  'input[name=operation]:checked'
+).value;
+
+//choice timer
+const choiceTimer = document.querySelector('input[name=time]:checked').value;
+
+//button play calcul
+const buttonPlay = document.querySelector('.play');
+
+// display timer
+const displayTimer = document.getElementById('timer');
+
+//display question calcul
+const displayQuestionCalcul = document.querySelector('.question-calcul');
+const responseCalcul = document.querySelector('.input-response');
