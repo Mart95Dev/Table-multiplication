c5b493781dd10d08eb5ef1bad8340bc1450c1b42
Martial Wehrling <d.wehrling@gmail.com>
2021-06-12 16:44:12 +0200

    script js table multiply and multiply random

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
+// button disabled : btn-number-disabled
+const blockTableMultiplyRandom = document.querySelector('.position-random');
+const blockTableMultiply = document.querySelector('fieldset');
+
 // choice table multiply
-const tableMultiply = document.querySelectorAll('.btn-number');
+
+const tablesMultiply = document.querySelectorAll('.btn-number');
 
 // choice table multiply random
 const buttonTableRandom = document.querySelector('.table-number-random');
-const tableMultiplyRandom = document.getElementById('multiply-random');
+const tableMultiplyRandom = document.querySelector('.multiply-random');
+const textMultiplyRandom = document.getElementById('text-multiply-random');
 
 //choice number operation
 const choiceOperation = document.querySelector(
   'input[name=operation]:checked'
 ).value;
 
-//choice timer
+//choice timer and display timer
 const choiceTimer = document.querySelector('input[name=time]:checked').value;
+const displayTimer = document.getElementById('timer');
 
 //button play calcul
-const buttonPlay = document.querySelector('.play');
-
-// display timer
-const displayTimer = document.getElementById('timer');
+const buttonPlayCalcul = document.querySelector('.play');
+const buttonResetGame = document.querySelector('.reset');
 
 //display question calcul
 const displayQuestionCalcul = document.querySelector('.question-calcul');
 const responseCalcul = document.querySelector('.input-response');
+
+//variables
+let multiplyComputer = 0;
+let choiceMultiply = 0;
+
+// choice table multiply
+tablesMultiply.forEach((table) => {
+  table.addEventListener('click', (e) => {
+    let buttonNumber = e.target.id;
+    choiceMultiply = buttonNumber;
+    if (buttonNumber !== '') {
+      blockTableMultiplyRandom.classList.add('boxDisabled');
+    }
+  });
+});
+
+// table multiply random
+buttonTableRandom.addEventListener('click', () => {
+  multiplyComputer = Math.floor(Math.random() * 14) + 2; // +2 pour éliminer le 0 et 1
+  blockTableMultiply.classList.add('boxDisabled');
+  tableMultiplyRandom.classList.add('random-active');
+  const numberRandom = multiplyComputer;
+  textMultiplyRandom.textContent = numberRandom;
+});
