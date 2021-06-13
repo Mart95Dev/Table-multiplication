5fd52b623423a80850c3d1074acbe066f713871d
Martial Wehrling <d.wehrling@gmail.com>
2021-06-13 12:52:29 +0200

    create function  button reset and function box-disabled

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// button disabled : btn-number-disabled
+// button disabled : boxDisabled
 const blockTableMultiplyRandom = document.querySelector('.position-random');
 const blockTableMultiply = document.querySelector('fieldset');
 
@@ -12,12 +12,10 @@ const tableMultiplyRandom = document.querySelector('.multiply-random');
 const textMultiplyRandom = document.getElementById('text-multiply-random');
 
 //choice number operation
-const choiceOperation = document.querySelector(
-  'input[name=operation]:checked'
-).value;
+const choiceOperations = document.querySelectorAll('input[name="operation"]');
 
 //choice timer and display timer
-const choiceTimer = document.querySelector('input[name=time]:checked').value;
+const choiceTimes = document.querySelectorAll('input[name="time"]');
 const displayTimer = document.getElementById('timer');
 
 //button play calcul
@@ -31,23 +29,62 @@ const responseCalcul = document.querySelector('.input-response');
 //variables
 let multiplyComputer = 0;
 let choiceMultiply = 0;
+let numberOperation = 0;
+let second = 0;
 
 // choice table multiply
 tablesMultiply.forEach((table) => {
   table.addEventListener('click', (e) => {
-    let buttonNumber = e.target.id;
+    const buttonNumber = e.target.id;
     choiceMultiply = buttonNumber;
-    if (buttonNumber !== '') {
-      blockTableMultiplyRandom.classList.add('boxDisabled');
-    }
+    tableDisabled();
   });
 });
 
-// table multiply random
+// choice table multiply random
 buttonTableRandom.addEventListener('click', () => {
   multiplyComputer = Math.floor(Math.random() * 14) + 2; // +2 pour éliminer le 0 et 1
-  blockTableMultiply.classList.add('boxDisabled');
-  tableMultiplyRandom.classList.add('random-active');
+  tableDisabled();
   const numberRandom = multiplyComputer;
   textMultiplyRandom.textContent = numberRandom;
 });
+
+// choice operation number
+choiceOperations.forEach((operation) => {
+  operation.addEventListener('click', (e) => {
+    numberOperation = e.target.id;
+    operation.setAttribute('checked', '');
+  });
+});
+
+//choice timer
+choiceTimes.forEach((time) => {
+  time.addEventListener('click', (e) => {
+    second = e.target.id;
+    time.setAttribute('checked', '');
+  });
+});
+
+//button reset
+buttonResetGame.addEventListener('click', reset);
+
+//box-disabled
+function tableDisabled() {
+  if (choiceMultiply > 0) {
+    blockTableMultiplyRandom.classList.add('boxDisabled');
+  } else if (multiplyComputer > 0) {
+    blockTableMultiply.classList.add('boxDisabled');
+    tableMultiplyRandom.classList.add('random-active');
+  } else {
+    return;
+  }
+}
+
+//function reset table disabled
+function reset() {
+  document.querySelector('input[name="operation"]:checked').checked = false;
+  document.querySelector('input[name="time"]:checked').checked = false;
+  blockTableMultiply.classList.remove('boxDisabled');
+  blockTableMultiplyRandom.classList.remove('boxDisabled');
+  return;
+}
